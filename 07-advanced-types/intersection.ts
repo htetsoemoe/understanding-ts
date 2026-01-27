eebb16b1a9d965849f59caad7245c470cb686958
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-01-27 07:05:18 +0630

    advanced types

---
+++ 07-advanced-types/intersection.ts
@@ -0,0 +1,19 @@
+// Intersection
+
+type FileData = {
+    path: string,
+    content: string,
+}
+
+type DatabaseData = {
+    connectionUrl: string,
+    credentials: string,
+}
+
+type Status = {
+    isOpen: boolean,
+    errorMessage?: string,
+}
+
+type AccessedFileData = FileData & Status
+type AccessedDatabaseData = DatabaseData & Status 
