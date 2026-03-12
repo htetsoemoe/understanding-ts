cc5dafe92281ce5cc6df77a07079d41670857720
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-12 21:19:18 +0630

    autobind decorator

--- 13-drag-and-drop-project/src/app.ts
+++ 13-drag-and-drop-project/src/app.ts
@@ -1,3 +1,17 @@
+// autobind decorator to automatically bind 'this' context
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
@@ -32,6 +46,7 @@ class ProjectInput {
     }
 
     // Method to handle form submission
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault()
         console.log(this.titleInputElement.value)
@@ -39,7 +54,9 @@ class ProjectInput {
 
     // Method to configure event listeners for the form
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this))
+        this.element.addEventListener('submit', this.submitHandler)
+
+        // this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     // Method to attach the form element to the DOM
