530f6e4ce9e416cc249ef7e5b4d65c479985f36c
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-14 11:07:37 +0630

    input validations

--- 13-drag-and-drop-project/src/app.ts
+++ 13-drag-and-drop-project/src/app.ts
@@ -3,12 +3,54 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value
     const adjDescriptor: PropertyDescriptor = {
         configurable: true,
-        get() {
+        get() { 
             const boundFn = originalMethod.bind(this)
             return boundFn
         }
     }
     return adjDescriptor
+}
+
+// Validation 
+interface Validatable {
+    value: string | number
+    required?: boolean
+    minLength?: number
+    maxLength?: number
+    min?: number
+    max?: number
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    if (
+        validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+    }
+    if (
+        validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = isValid && validatableInput.value.length >= validatableInput.maxLength
+    }
+    if (
+        validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    if (
+        validatableInput.max != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    return isValid
 }
 
 // ProjectInput Class
@@ -45,13 +87,60 @@ class ProjectInput {
         this.attach()
     }
 
+    // Method to gather user input from the form fields
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.descriptionInputElement.value
+        const enteredPeople = +this.peopleInputElement.value // Convert string to number
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        }
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        }
+
+        const peopleValidatable: Validatable = {
+            value: enteredPeople,
+            required: true,
+            min: 1,
+            max: 50
+        }
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!')
+            return
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    // Method to clear the input fields after submission
+    private clearInputs() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.peopleInputElement.value = ''
+    }
+
     // Method to handle form submission
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault()
-        console.log(this.titleInputElement.value)
+        const userInput = this.gatherUserInput()
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput
+            console.log(title, desc, people)
+            this.clearInputs()
+        }
     }
-
+  
     // Method to configure event listeners for the form
     private configure() {
         this.element.addEventListener('submit', this.submitHandler)
