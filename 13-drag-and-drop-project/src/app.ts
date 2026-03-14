12af51b1270bec311110de2db6cc49dd2f9ddf03
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-14 15:13:08 +0630

    more classes and custom types

--- 13-drag-and-drop-project/src/app.ts
+++ 13-drag-and-drop-project/src/app.ts
@@ -1,7 +1,27 @@
+// Project Status Type
+enum ProjectStatus {
+    Active,
+    Finished
+}
+
+// Project Class
+class Project {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: ProjectStatus
+    ) {}
+}
+
+type Listener = (items: Project[]) => void
+
+
 // Project State Management
 class ProjectState {
-    private listeners: any[] = []
-    private projects: any[] = []
+    private listeners: Listener[] = []
+    private projects: Project[] = []
     private static instance: ProjectState
 
     private constructor() {}
@@ -14,17 +34,18 @@ class ProjectState {
         return this.instance
     }
 
-    addListener(listenerFn: Function) {
+    addListener(listenerFn: Listener) {
         this.listeners.push(listenerFn)
     }
 
     addProject(title: string, description: string, numOfPeople: number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title: title,
-            description: description,
-            people: numOfPeople
-        }
+        const newProject = new Project(
+            Math.random().toString(),
+            title,
+            description,
+            numOfPeople,
+            ProjectStatus.Active
+        )
         this.projects.push(newProject)
         // Call all listener functions to update the UI
         for (const listenerFn of this.listeners) {
