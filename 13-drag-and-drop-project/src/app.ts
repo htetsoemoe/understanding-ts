921da6c0281262a5258aea16c70d89b58d4b7f0a
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-14 13:05:36 +0630

    render project lists

--- 13-drag-and-drop-project/src/app.ts
+++ 13-drag-and-drop-project/src/app.ts
@@ -53,6 +53,43 @@ function validate(validatableInput: Validatable) {
     return isValid
 }
 
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            'project-list'
+        )! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        )
+
+        // Get the first element from the imported content and cast it to HTMLElement
+        // first element is the <section> element in the template
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type}-projects`
+
+        this.attach()
+        this.renderContent()
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
+        this.element.querySelector('ul')!.id = listId
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+    }
+}
+
 // ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement
@@ -155,3 +192,6 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput()
+
+const activePrjList = new ProjectList('active')
+const finishedPrjList = new ProjectList('finished')
