0dfdf592301077983679cca38cb64086ea2ce854
htetsoemoe <soemoehtet.mdy@gmail.com>
2026-03-30 15:20:01 +0630

    build tool vite

---
+++ 15.build-tool-vite/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className='w-screen h-screen mx-auto bg-blue-950'>
+      <div className="pt-5 pl-5">
+        <p className="text-2xl text-white ">
+          Hello From React Project with TypeScript, Vite, Tailwind CSS
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
